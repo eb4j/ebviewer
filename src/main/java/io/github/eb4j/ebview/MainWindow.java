fe964eb7ede95e8c5ba0317da627f621bf5bf99d
Hiroshi Miura <miurahr@linux.com>
2021-08-30 11:25:45 +0900

    Add history pane

--- src/main/java/io/github/eb4j/ebview/MainWindow.java
+++ src/main/java/io/github/eb4j/ebview/MainWindow.java
@@ -1,17 +1,18 @@
 package io.github.eb4j.ebview;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.border.TitledBorder;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 
 /**
@@ -23,10 +24,13 @@ public class MainWindow extends JFrame {
     private JTextField searchWordField;
     private JButton searchButton;
     private JScrollPane articlePane;
+    private TitledBorder border;
+    private JScrollPane historyPane;
+    private DefaultListModel<String> historyModel = new DefaultListModel<>();
 
     public MainWindow(final EBDict ebDict) {
         super("EBViewer");
-        setPreferredSize(new Dimension(415, 405));
+        setPreferredSize(new Dimension(515, 405));
         setLayout(new BorderLayout());
         //
         panel1 = new JPanel();
@@ -42,22 +46,43 @@ public class MainWindow extends JFrame {
         articlePane = new JScrollPane(dictionaryPane);
         articlePane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         //
+        border = new TitledBorder("History");
+        border.setTitleJustification(TitledBorder.CENTER);
+        border.setTitlePosition(TitledBorder.TOP);
+
+        JList<String> history = new JList<>(historyModel);
+        historyPane = new JScrollPane(history);
+        historyPane.setPreferredSize(new Dimension(100, -1));
+        historyPane.setBorder(border);
+        //
         getContentPane().add(panel1, BorderLayout.NORTH);
         getContentPane().add(articlePane, BorderLayout.CENTER);
+        getContentPane().add(historyPane, BorderLayout.EAST);
         //
-        searchButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                String word = searchWordField.getText();
-                new Thread(() -> {
-                    List<DictionaryEntry> result = ebDict.readArticles(word);
-                    SwingUtilities.invokeLater(() -> dictionaryPane.setFoundResult(result));
-                }).start();
+        searchWordField.addActionListener(e -> {
+            String word = searchWordField.getText();
+            historyModel.add(0, word);
+            new Thread(() -> {
+                List<DictionaryEntry> result = ebDict.readArticles(word);
+                SwingUtilities.invokeLater(() -> dictionaryPane.setFoundResult(result));
+            }).start();
+        });
+        searchButton.addActionListener(e -> {
+            String word = searchWordField.getText();
+            historyModel.add(0, word);
+            new Thread(() -> {
+                List<DictionaryEntry> result = ebDict.readArticles(word);
+                SwingUtilities.invokeLater(() -> dictionaryPane.setFoundResult(result));
+            }).start();
+        });
+        history.addListSelectionListener(e -> {
+            if (e.getValueIsAdjusting()) {
+                // The user is still manipulating the selection.
+                return;
+            }
+            Object obj = history.getSelectedValue();
+            if (obj != null) {
+                searchWordField.setText(obj.toString());
             }
         });
         //
