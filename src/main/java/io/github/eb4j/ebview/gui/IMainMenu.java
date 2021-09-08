ff798621d5d68c0749ed4e04706cacfeb98c52d5
Hiroshi Miura <miurahr@linux.com>
2021-09-08 17:02:00 +0900

    Adjust spotbugs
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ src/main/java/io/github/eb4j/ebview/gui/IMainMenu.java
@@ -0,0 +1,6 @@
+package io.github.eb4j.ebview.gui;
+
+public interface IMainMenu {
+
+    void invokeAction(String action, int modifiers);
+}
--- src/main/java/io/github/eb4j/ebview/utils/LStrings.java
+++ src/main/java/io/github/eb4j/ebview/utils/LStrings.java
@@ -7,13 +7,6 @@ public final class LStrings {
     /** Resource bundle that contains all the strings */
     private static ResourceBundle bundle = ResourceBundle.getBundle("Bundle");
 
-    /**
-     * Returns resource bundle.
-     */
-    public static ResourceBundle getResourceBundle() {
-        return bundle;
-    }
-
     /** Returns a localized String for a key */
     public static String getString(String key) {
         return bundle.getString(key);
