82f6c3a5b462d5648a5e88cf4ff09b6f04fbe12f
Hiroshi Miura <miurahr@linux.com>
2021-09-10 09:53:33 +0900

    Update test expectation

--- src/test/java/io/github/eb4j/ebview/dictionary/epwing/GaijiTest.java
+++ src/test/java/io/github/eb4j/ebview/dictionary/epwing/GaijiTest.java
@@ -15,6 +15,6 @@ public class GaijiTest {
                 0x00, 0x00, 0x00, 0x03, (byte)0xc0, 0x04, 0x20 , 0x00, 0x20, 0x00,
                 (byte)0xe0, 0x03, 0x20, 0x04, 0x20, 0x04, 0x60, 0x03, (byte)0xa0, 0x00, 0x00, 0x00, 0x00};
         String image = Utils.convertMonoGraphic2Base64(data, 16, 16);
-        assertEquals("<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEX///8AAABVwtN+AAAAKklEQVR4XmNggIAHzMz8EBYj815GFhmmPwxMDAcYmWWZGIEMB6b/DhBZAHKDBT9OJvDqAAAAAElFTkSuQmCC\"/>", image);
+        assertEquals("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEX///8AAABVwtN+AAAAKklEQVR4XmNggIAHzMz8EBYj815GFhmmPwxMDAcYmWWZGIEMB6b/DhBZAHKDBT9OJvDqAAAAAElFTkSuQmCC", image);
     }
 }
