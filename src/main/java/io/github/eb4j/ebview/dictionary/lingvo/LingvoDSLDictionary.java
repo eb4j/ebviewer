3f38fe670e8d4e9d3c5c072e7dbc707bd4fe0b0d
Hiroshi Miura <miurahr@linux.com>
2021-09-27 17:14:43 +0900

    DSL: support image tag(wip)
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/java/io/github/eb4j/ebview/dictionary/lingvo/LingvoDSLDictionary.java
+++ src/main/java/io/github/eb4j/ebview/dictionary/lingvo/LingvoDSLDictionary.java
@@ -179,6 +179,9 @@ public class LingvoDSLDictionary implements IDictionary {
             reList.add(new RE("\\[s\\](.+?\\.wav)\\[/s\\]", "<a href=\"file://@dir@/$1\"/>SOUND: $1</a>"));
             reList.add(new RE("\\[s\\](.+?\\.jpg)\\[/s\\]", "<img src=\"file://@dir@/$1\"/>"));
             reList.add(new RE("\\[s\\](.+?\\.bmp)\\[/s\\]", "<img src=\"file://@dir@/$1\"/>"));
+            reList.add(new RE("\\[s\\](.+?\\.tif)\\[/s\\]", "<img src=\"file://@dir@/$1\"/>"));
+            reList.add(new RE("\\[s\\](.+?\\.tiff)\\[/s\\]", "<img src=\"file://@dir@/$1\"/>"));
+            reList.add(new RE("\\[s\\](.+?\\.png)\\[/s\\]", "<img src=\"file://@dir@/$1\"/>"));
             reList.add(new RE("\\[video\\](.+?)\\[/video\\]", "<a href=\"file://@dir@/$1\">VIDEO: $1</a>"));
             reList.add(new RE("\\[s\\](.+?)\\[/s\\]", "UNSUPPORTED MEDIA: $1"));
             reList.add(new RE("\\[sub\\](.+?)\\[/sub\\]", "<sub>$1</sub>"));
