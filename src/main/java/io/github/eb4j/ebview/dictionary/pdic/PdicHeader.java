8aa895374bc1e25ac5049dc71fb0f67a8223701f
Hiroshi Miura <miurahr@linux.com>
2021-09-29 00:10:52 +0900

    PDIC: adjust spotbugs warnings
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/java/io/github/eb4j/ebview/dictionary/pdic/PdicHeader.java
+++ src/main/java/io/github/eb4j/ebview/dictionary/pdic/PdicHeader.java
@@ -11,17 +11,16 @@ final class PdicHeader {
     private final static int L_HEADERNAME = 100;    //   ヘッダー部文字列長
     private final static int L_DICTITLE = 40;        //   辞書タイトル名長
 
-    public String headername; //   辞書ヘッダータイトル
+    // public String headername; //   辞書ヘッダータイトル
     // public String dictitle;   //   辞書名
     public short version;      //   辞書のバージョン
-    public short lword;        //   見出語の最大長
-    public short ljapa;        //   訳語の最大長
+    // public short lword;        //   見出語の最大長
+    // public short ljapa;        //   訳語の最大長
     public short block_size;      //  (256 ) １ブロックのバイト数 固定
     public short index_block;     //   インデックスブロック数
     public short header_size;     //   ヘッダーのバイト数
-    public short index_size;  //  ( ) インデックスのバイト数 未使用
+    // public short index_size;  //  ( ) インデックスのバイト数 未使用
 
-    public short empty_block;     // 空きブロックの先頭物理ブロック番号（ないときは-1  ）
     public short nindex;       //  ( ) インデックスの要素の数 未使用
     public short nblock;       //  ( ) 使用データブロック数 未使用
     public int nword;    //   登録単語数
@@ -31,16 +30,8 @@ final class PdicHeader {
 
     public byte attrlen;       //   単語属性の長さ
     public byte os;          // OS
-    public int olenumber;      // OLE  用シリアル番号
-    public short lid_word;      //  ID 見出語言語
-
-    public short lid_japa;      //  ID 訳語部言語
-    public short lid_exp;      //  ID 用例部言語
-    public short lid_pron;      //  ID 発音記号言語
-    public short lid_other;      //  ID その他言語
     public boolean index_blkbit;   // false:16bit, true:32bit
     public int extheader;      //   拡張ヘッダーサイズ
-    public int empty_block2;     //   空きブロック先頭物理ブロック番号
     public int nindex2;       //   インデックス要素の数
     public int nblock2;       //   使用データブロック数
 
@@ -59,7 +50,7 @@ final class PdicHeader {
      */
     public int load(ByteBuffer header_block) throws RuntimeException {
         int ret = 0;
-        Charset sjisset = Charset.forName("X-SJIS");
+        // Charset sjisset = Charset.forName("X-SJIS");
 
         byte[] headernamebuff = new byte[L_HEADERNAME];
         byte[] dictitlebuff = new byte[L_DICTITLE];
@@ -67,41 +58,42 @@ final class PdicHeader {
         header_block.flip();
         header_block.order(ByteOrder.LITTLE_ENDIAN);
         header_block.get(headernamebuff);
-        headername = sjisset.decode(ByteBuffer.wrap(headernamebuff))
-                .toString();
+        // headername = sjisset.decode(ByteBuffer.wrap(headernamebuff)).toString();
         header_block.get(dictitlebuff);
         // dictitle = sjisset.decode(ByteBuffer.wrap(dictitlebuff)).toString();
         version = header_block.getShort();
         if ((version & 0xFF00) == 0x0500 || (version & 0xFF00) == 0x0600) {
-            lword = header_block.getShort();
-            ljapa = header_block.getShort();
+            header_block.getShort();  // lword
+            header_block.getShort();  // ljapa
 
             block_size = header_block.getShort();
             index_block = header_block.getShort();
             header_size = header_block.getShort();
-            index_size = header_block.getShort();
-            empty_block = header_block.getShort();
-            nindex = header_block.getShort();
-            nblock = header_block.getShort();
+            header_block.getShort();  // index_size
+            header_block.getShort();  // empty_block
+            header_block.getShort();  // nindex
+            header_block.getShort();  // nblock
 
-            nword = header_block.getInt();
+            header_block.getInt();  // nword
 
-            dicorder = header_block.get();
-            dictype = header_block.get();
+            header_block.get();  // dicorder
+            header_block.get();  // dictype
             attrlen = header_block.get();
             os = header_block.get();
 
-            olenumber = header_block.getInt();
-            lid_word = header_block.getShort();
+            header_block.getInt();  // ole_number
 
-            lid_japa = header_block.getShort();
-            lid_exp = header_block.getShort();
-            lid_pron = header_block.getShort();
-            lid_other = header_block.getShort();
+            // lid_dummy
+            header_block.getShort();
+            header_block.getShort();
+            header_block.getShort();
+            header_block.getShort();
+            header_block.getShort();
+
             index_blkbit = (header_block.get() != 0);
             header_block.get(); // dummy0
             extheader = header_block.getInt();
-            empty_block2 = header_block.getInt();
+            header_block.getInt();  //empty_block2
             nindex2 = header_block.getInt();
             nblock2 = header_block.getInt();
 
