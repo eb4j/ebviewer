d4dab41161ffa162321868b4ab515a5225320d5b
Hiroshi Miura <miurahr@linux.com>
2021-10-09 14:25:56 +0900

    Update LingvoDSL driver
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/java/io/github/eb4j/ebview/dictionary/lingvo/LingvoDSLDictionary.java
+++ src/main/java/io/github/eb4j/ebview/dictionary/lingvo/LingvoDSLDictionary.java
@@ -13,9 +13,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -32,6 +33,10 @@ import java.util.zip.GZIPInputStream;
  * @author Hiroshi Miura
  */
 public class LingvoDSLDictionary implements IDictionary {
+
+    // An ordered list of Pair of Regex pattern and replacement string
+    private static final TreeMap<Pattern, String> TAG_REPLACEMENTS = new TreeMap<>(
+            Comparator.comparing(Pattern::pattern));
 
     protected final DictionaryData<String> data;
     private final String dictionaryDir;
@@ -66,12 +71,16 @@ public class LingvoDSLDictionary implements IDictionary {
         }
     }
 
+    private static boolean testLine(final String line) {
+        return !line.isEmpty() && !line.startsWith("#");
+    }
+
     private void loadData(final Stream<String> stream) {
         StringBuilder word = new StringBuilder();
         StringBuilder trans = new StringBuilder();
-        stream.filter(line -> !line.isEmpty() && !line.startsWith("#"))
-                .map(LingvoDSLTag::replaceTag)
-                .forEach(line -> {
+        stream.filter(LingvoDSLDictionary::testLine)
+              .map(LingvoDSLDictionary::replaceTag)
+              .forEach(line -> {
                     if (Character.isWhitespace(line.codePointAt(0))) {
                         trans.append(line.trim()).append('\n');
                     } else {
@@ -96,13 +105,17 @@ public class LingvoDSLDictionary implements IDictionary {
 
     @Override
     public List<DictionaryEntry> readArticles(final String word) {
-        return data.lookUp(word).stream().map(e -> new DictionaryEntry(e.getKey(), e.getValue().replaceAll("@dir@", dictionaryDir), bookName))
+        return data.lookUp(word).stream()
+                .map(e -> new DictionaryEntry(e.getKey(), e.getValue()
+                        .replaceAll("@dir@", dictionaryDir), bookName))
                 .collect(Collectors.toList());
     }
 
     @Override
     public List<DictionaryEntry> readArticlesPredictive(final String word) {
-        return data.lookUpPredictive(word).stream().map(e -> new DictionaryEntry(e.getKey(), e.getValue().replaceAll("@dir@", dictionaryDir), bookName))
+        return data.lookUpPredictive(word).stream()
+                .map(e -> new DictionaryEntry(e.getKey(), e.getValue()
+                        .replaceAll("@dir@", dictionaryDir), bookName))
                 .collect(Collectors.toList());
     }
 
@@ -113,93 +126,69 @@ public class LingvoDSLDictionary implements IDictionary {
     public void close() {
     }
 
-    @SuppressWarnings("visibilitymodifier")
-    static class RE {
-        public Pattern pattern;
-        public String replacement;
-
-        RE(final String regex, final String replacement) {
-            pattern = Pattern.compile(regex);
-            this.replacement = replacement;
+    private static String replaceTag(final String line) {
+        String result = line;
+        for (Map.Entry<Pattern, String> entry : TAG_REPLACEMENTS.entrySet()) {
+            result = entry.getKey().matcher(result).replaceAll(entry.getValue());
         }
+        return result;
     }
 
-    static class LingvoDSLTag {
-        private static final List<RE> RE_LIST;
-
-        static String replaceTag(final String line) {
-            String result = line;
-            for (RE re : RE_LIST) {
-                result = re.pattern.matcher(result).replaceAll(re.replacement);
-            }
-            return result;
-        }
-
-        /**
-         * Initialize regex patterns as an immutable list.
-         */
-        static {
-            List<RE> reList = new ArrayList<>();
-            reList.add(new RE("\\[\\[(.+?)\\]\\]", "&#91;$1&#93;"));
-            reList.add(new RE("\\\\\\[", "&#91;"));
-            reList.add(new RE("\\\\\\]", "&#93;"));
-            reList.add(new RE("\\[b\\](.+?)\\[/b\\]", "<span style='font-style: bold'>$1</span>"));
-            reList.add(new RE("\\[i\\](.+?)\\[/i\\]", "<span style='font-style: italic'>$1</span>"));
-            reList.add(new RE("\\[trn\\](.+?)\\[/trn\\]", "$1"));
-            reList.add(new RE("\\[t\\](.+?)\\[/t\\]", "$1&nbsp;"));
-            reList.add(new RE("\\[br\\]", "<br/>"));
-            // Green is default color in Lingvo
-            reList.add(new RE("\\[c\\](.+?)\\[/c\\]", "<span style='color:green'>$1</span>"));
-            // The following line tries to replace [c value]text[/c] with text colored as per the value.
-            // Since the color names are plain words like 'red', or 'blue', or 'steelgray' etc.,
-            // FIXME: I use the ([a-z]+?) regular expression, but am not sure if it is correct.
-            reList.add(new RE("\\[c\\s([a-z]+?)\\](.+?)\\[/c\\]", "<span style='color:$1'>$2</span>"));
-            reList.add(new RE("\\[com\\]", ""));
-            reList.add(new RE("\\[/com\\]", ""));
-            reList.add(new RE("\\[ex\\]", ""));
-            reList.add(new RE("\\[/ex\\]", ""));
-            reList.add(new RE("\\[lang\\]", ""));
-            reList.add(new RE("\\[/lang\\]", ""));
-            reList.add(new RE("\\[m\\](.+?)\\[/m\\]", "$1"));
-            reList.add(new RE("\\[m1\\](.+?)\\[/m\\]", "<div style=\"text-indent: 30px\">$1</div>"));
-            reList.add(new RE("\\[m2\\](.+?)\\[/m\\]", "<div style=\"text-indent: 60px\">$1</div>"));
-            reList.add(new RE("\\[m3\\](.+?)\\[/m\\]", "<div style=\"text-indent: 90px\">$1</div>"));
-            reList.add(new RE("\\[m4\\](.+?)\\[/m\\]", "<div style=\"text-indent: 90px\">$1</div>"));
-            reList.add(new RE("\\[m5\\](.+?)\\[/m\\]", "<div style=\"text-indent: 90px\">$1</div>"));
-            reList.add(new RE("\\[m6\\](.+?)\\[/m\\]", "<div style=\"text-indent: 90px\">$1</div>"));
-            reList.add(new RE("\\[m7\\](.+?)\\[/m\\]", "<div style=\"text-indent: 90px\">$1</div>"));
-            reList.add(new RE("\\[m8\\](.+?)\\[/m\\]", "<div style=\"text-indent: 90px\">$1</div>"));
-            reList.add(new RE("\\[m9\\](.+?)\\[/m\\]", "<div style=\"text-indent: 90px\">$1</div>"));
-            reList.add(new RE("\\[p\\]", ""));
-            reList.add(new RE("\\[/p\\]", ""));
-            reList.add(new RE("\\[preview\\]", ""));
-            reList.add(new RE("\\[/preview\\]", ""));
-            reList.add(new RE("\\[ref\\]", ""));
-            reList.add(new RE("\\[/ref\\]", ""));
-            reList.add(new RE("\\[s\\](.+?\\.(wav|WAV))\\[/s\\]", "<a href=\"file://@dir@/$1\">SOUND: $1</a>"));
-            reList.add(new RE("\\[s\\](.+?\\.(jpg|JPG))\\[/s\\]", "<img src=\"file://@dir@/$1\"/>"));
-            reList.add(new RE("\\[s\\](.+?\\.(png|PNG))\\[/s\\]", "<img src=\"file://@dir@/$1\"/>"));
-            reList.add(new RE("\\[video\\](.+?)\\[/video\\]", "<a href=\"file://@dir@/$1\">VIDEO: $1</a>"));
-            reList.add(new RE("\\[s\\](.+?)\\[/s\\]", "<a href=\"file://@dir@/$1\">MEDIA: $1</a>"));
-            reList.add(new RE("\\[sub\\](.+?)\\[/sub\\]", "<sub>$1</sub>"));
-            reList.add(new RE("\\[sup\\](.+?)\\[/sup\\]", "<sup>$1</sup>"));
-            reList.add(new RE("\\[trn1\\]", ""));
-            reList.add(new RE("\\[/trn1\\]", ""));
-            reList.add(new RE("\\[trs\\]", ""));
-            reList.add(new RE("\\[/trs\\]", ""));
-            // FIXME: In the following two lines, the exclamation marks are escaped. Maybe, it is superfluous.
-            reList.add(new RE("\\[\\!trs\\]", ""));
-            reList.add(new RE("\\[/\\!trs\\]", ""));
-            reList.add(new RE("\\[u\\](.+?)\\[/u\\]",
-                    "<span style='text-decoration:underline'>$1</span>"));
-            reList.add(new RE("\\[url\\](.+?)\\[/url\\]", "<a href='$1'>$1</a>"));
-            // The following line tries to replace a letter surrounded by ['][/'] tags (indicating stress)
-            // with a red letter (the default behavior in Lingvo).
-            reList.add(new RE("\\['\\].\\[/'\\]", "<span style='color:red'>$1</span>"));
-            // FIXME: In the following two lines, the asterisk symbols are escaped. Maybe, it is superfluous.
-            reList.add(new RE("\\[\\*\\]", ""));
-            reList.add(new RE("\\[/\\*\\]", ""));
-            RE_LIST = Collections.unmodifiableList(reList);
+    static {
+        // TODO: real parser.
+        // DSL language is not context-free:
+        // it allows arbitrary nesting (and also the meaning of e.g. [/m] is context-dependent)
+        // so this approach of replacing regex patterns is doomed to fail at at least some cases.
+        // and may leave some [tag].
+        // The only way to handle this language robustly is to make a real parser.
+        // ---
+        // following 3 lines replaces "[[...]]" and "\[..\]" into "[...]"
+        // but using HTML body reference to avoid further match.
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[\\[(?<content>.+?)]]"), "&#91;${content}&#93;");
+        TAG_REPLACEMENTS.put(Pattern.compile(Pattern.quote("\\[")), "&#91;");
+        TAG_REPLACEMENTS.put(Pattern.compile(Pattern.quote("\\]")), "&#93;");
+        // styling tags
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[b](?<content>.+?)\\[/b]"), "<strong>${content}</strong>");
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[i](?<content>.+?)\\[/i]"), "<span style='font-style: italic'>${content}</span>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[t](?<content>.+?)\\[/t]"), "${content}&nbsp;");
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[c](?<content>.+?)\\[/c]"), "<span style='color:green'>${content}</span>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[u](?<content>.+?)\\[/u]"),
+                "<span style='text-decoration:underline'>${content}</span>");
+        // The following line tries to replace [c value]text[/c] with text colored as per the value.
+        // Since the color names are plain words like 'red', or 'blue', or 'steelgray' etc.,
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[c\\s(?<color>[a-z]+?)](?<content>.+?)\\[/c]"), "<span style='color:${color}'>${content}</span>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[sub](?<content>.+?)\\[/sub]"), "<sub>${content}</sub>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[sup](?<content>.+?)\\[/sup]"), "<sup>${content}</sup>");
+        // line feed and indents
+        TAG_REPLACEMENTS.put(Pattern.compile(Pattern.quote("[br]")), "<br/>");
+        // ignore tag 'm" but "m1" to indent 1 level, "m2" to indent 2 level and "m3" and more to indent 3 level.
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[m1](?<content>.+?)\\[/m]"), "<p style=\"text-indent: 30px\">${content}</p>");
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[m2](?<content>.+?)\\[/m]"), "<p style=\"text-indent: 60px\">${content}</p>");
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[(m3|m4|m5|m6|m7|m8|m9)](?<content>.+?)\\[/m]"), "<p style=\\\"text-indent: 90px\">${content}</p>");
+        // external link may launch external browser
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[url](?<link>.+?)\\[/url]"), "<a href='${link}'>LINK</a>");
+        // The following line tries to replace a letter surrounded by ['][/'] tags (indicating stress)
+        // with a red letter (the default behavior in Lingvo).
+        TAG_REPLACEMENTS.put(
+                Pattern.compile("\\['](?<content>.+?)\\[/']"), "<span style='color:red'>${content}</span>");
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[s](?<media>.+?\\.wav)\\[/s]"), "<a href=\"file://@dir@/${media}\">SOUND: $1</a>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[s](?<media>.+?\\.jpg)\\[/s]"), "<img src=\"file://@dir@/${media}\"/>");
+        TAG_REPLACEMENTS.put(Pattern.compile("\\[s](?<media>.+?\\.png)\\[/s]"), "<img src=\"file://@dir@/${media}\"/>");
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[video](?<media>.+?)\\[/video]"), "<a href=\"file://@dir@/${media}\">VIDEO</a>");
+        TAG_REPLACEMENTS.put(Pattern.compile(
+                "\\[s](?<media>.+?)\\[/s]"), "<a href=\"file://@dir@/${media}\">MEDIA: ${media}</a>");
+        // silently ignored these tags that can be arbitrary nested.
+        String[] ignoreTags = {"\\*", "m", "com", "ex", "lang", "p", "preview", "ref", "trn", "trn1", "trs", "!trs"};
+        for (String tag : ignoreTags) {
+            TAG_REPLACEMENTS.put(Pattern.compile("\\[(?<tag>" + tag + ")](?<content>.+?)\\[/\\k<tag>]"), "${content}");
         }
     }
 }
