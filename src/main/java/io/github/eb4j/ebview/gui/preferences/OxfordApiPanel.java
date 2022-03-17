417edea2f35e5c8044ccdf9709f95a8b413988d9
Hiroshi Miura <miurahr@linux.com>
2022-03-17 17:46:39 +0900

    Add Preferences configuration dialog
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ src/main/java/io/github/eb4j/ebview/gui/preferences/OxfordApiPanel.java
@@ -0,0 +1,63 @@
+/*
+ * EBViewer, a dictionary viewer application.
+ * Copyright (C) 2022 Hiroshi Miura.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.eb4j.ebview.gui.preferences;
+
+import io.github.eb4j.ebview.utils.LStrings;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class OxfordApiPanel extends JPanel {
+    /**
+     * Creates a new panel to configure oxford dictionaries API's ID and Key.
+     */
+    public OxfordApiPanel() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setMinimumSize(new java.awt.Dimension(250, 200));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JLabel desc = new JLabel();
+        desc.setText(LStrings.getString("PREFS_OXFORD_DESC"));
+        add(desc);
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
+        JLabel appIdLabel = new JLabel();
+        appIdLabel.setText(LStrings.getString("PREFS_OXFORD_APPID_LABEL"));
+        panel1.add(appIdLabel);
+        appIdTextField = new JTextField();
+        panel1.add(appIdTextField);
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.X_AXIS));
+        JLabel appKeyLabel = new JLabel();
+        appKeyLabel.setText(LStrings.getString("PREFS_OXFORD_APPKEY_LABEL"));
+        appKeyTextField = new JTextField();
+        panel2.add(appKeyLabel);
+        panel2.add(appKeyTextField);
+        add(panel1);
+        add(panel2);
+    }
+
+    JTextField appIdTextField;
+    JTextField appKeyTextField;
+
+}
---
+++ src/main/java/io/github/eb4j/ebview/gui/preferences/PreferenceController.java
@@ -0,0 +1,61 @@
+/*
+ * EBViewer, a dictionary viewer application.
+ * Copyright (C) 2022 Hiroshi Miura.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.eb4j.ebview.gui.preferences;
+
+import io.github.eb4j.ebview.utils.LStrings;
+import io.github.eb4j.ebview.utils.Preferences;
+import io.github.eb4j.ebview.utils.StaticUIUtils;
+
+import javax.swing.JDialog;
+import javax.swing.JTabbedPane;
+import java.awt.Window;
+
+public class PreferenceController {
+
+    private IPreferencesController secureStoreController;
+    private IPreferencesController oxfordApiController;
+
+    public void show(final Window parent) {
+        JDialog dialog = new JDialog(parent);
+        dialog.setTitle(LStrings.getString("PASSWORD_DIALOG_TITLE"));
+        dialog.setSize(400, 300);
+        dialog.setModal(true);
+        StaticUIUtils.setEscapeClosable(dialog);
+        PreferencePanel preferencePanel = new PreferencePanel();
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+        secureStoreController = new SecureStoreController();
+        tabbedPane.addTab("Credentials", secureStoreController.getGui());
+        oxfordApiController = new OxfordApiController();
+        tabbedPane.addTab("Oxford dictionaries", oxfordApiController.getGui());
+        preferencePanel.prefsPanel.add(tabbedPane);
+
+        preferencePanel.okButton.addActionListener(e -> {
+            secureStoreController.persist();
+            oxfordApiController.persist();
+            Preferences.save();
+            StaticUIUtils.closeWindowByEvent(dialog);
+        });
+        preferencePanel.cancelButton.addActionListener(e -> StaticUIUtils.closeWindowByEvent(dialog));
+
+        dialog.add(preferencePanel);
+        dialog.setVisible(true);
+    }
+
+}
